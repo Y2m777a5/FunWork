6d900dcb6095319f2b674cf918dd0420c672f030
Y2m777a5 <orgdotin222@gmail.com>
2025-04-19 04:32:21 +0600

    New way to print Hello world
    Using something new to Print Hello world

---
+++ HelloWorld_2.0/HelloWorld_2_O.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class HelloWorld_2_O{
+    public static void main(String [] arg) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String (like \"HelloWorld\")");
+        String str = sc.nextLine();
+        String concat = "";
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            String temp = ""+ch;
+            if(ch == ' '){
+                concat+=temp;
+                continue;
+            }
+            System.out.print(concat);
+            if((int)ch>=(int)'1' && (int)ch<=(int)'9'){
+                for(int j = (int)'1'; j<((int)ch)+1; j++){
+                    Thread.sleep(150);
+                    System.out.print((char)j);
+                    System.out.print('\b');
+                }
+            }
+            else if((int)ch>=(int)'A' && (int)ch<=(int)'Z'){
+                for(int j = (int)'A'; j<((int)ch)+1; j++){
+                    Thread.sleep(150);
+                    System.out.print((char)j);
+                    System.out.print('\b');
+                }
+            }
+            else{
+                for(int j = (int)'a'; j<((int)ch)+1; j++){
+                    Thread.sleep(150);
+                    System.out.print((char)j);
+                    System.out.print('\b');
+                }
+            }
+            concat+=temp;
+            System.out.println();
+            sc.close();
+        }
+    }
+}
