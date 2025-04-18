66043130360bfb70e4cdbb43c82179beb1270137
Y2m777a5 <orgdotin222@gmail.com>
2025-04-19 05:07:19 +0600

    Print HelloWorld in a new way

--- Print_HelloWorld/HelloWorld_2_O.java
+++ Print_HelloWorld/HelloWorld_2_O.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.Thread;
 public class HelloWorld_2_O{
     public static void main(String [] arg) throws Exception{
         //throws Exception used in method declarations to specify that the method can throw one or more exceptions. 
@@ -17,7 +18,7 @@ public class HelloWorld_2_O{
             System.out.print(concat);
             if((int)ch>=(int)'1' && (int)ch<=(int)'9'){
                 for(int j = (int)'1'; j<((int)ch)+1; j++){
-                    Thread.sleep(150); 
+                    Thread.sleep(100);
                     //Thread class contains the sleep() method. Thread.sleep() pauses the current thread’s execution.
                     //For more:-[ https://www.geeksforgeeks.org/thread-sleep-method-in-java-with-examples/ ]
                     System.out.print((char)j);
@@ -26,14 +27,14 @@ public class HelloWorld_2_O{
             }
             else if((int)ch>=(int)'A' && (int)ch<=(int)'Z'){
                 for(int j = (int)'A'; j<((int)ch)+1; j++){
-                    Thread.sleep(150);
+                    Thread.sleep(100);
                     System.out.print((char)j);
                     System.out.print('\b');
                 }
             }
             else{
                 for(int j = (int)'a'; j<((int)ch)+1; j++){
-                    Thread.sleep(150);
+                    Thread.sleep(100);
                     System.out.print((char)j);
                     System.out.print('\b');
                 }
