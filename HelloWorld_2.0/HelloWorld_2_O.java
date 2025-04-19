890d942df60e854abc158732011793a871c45352
Y2m777a5 <orgdotin222@gmail.com>
2025-04-19 10:56:31 +0600

    Update HelloWorld_2_O.java

--- HelloWorld_2.0/HelloWorld_2_O.java
+++ HelloWorld_2.0/HelloWorld_2_O.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 public class HelloWorld_2_O{
     public static void main(String [] arg) throws Exception{
+        //throws Exception used in method declarations to specify that the method can throw one or more exceptions. 
+        //It provides a way for methods to signal that they may encounter exceptional conditions that they are not equipped to handle.
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter String (like \"HelloWorld\")");
         String str = sc.nextLine();
@@ -15,7 +17,9 @@ public class HelloWorld_2_O{
             System.out.print(concat);
             if((int)ch>=(int)'1' && (int)ch<=(int)'9'){
                 for(int j = (int)'1'; j<((int)ch)+1; j++){
-                    Thread.sleep(150);
+                    Thread.sleep(150); 
+                    //Thread class contains the sleep() method. Thread.sleep() pauses the current thread’s execution.
+                    //For more:-[ https://www.geeksforgeeks.org/thread-sleep-method-in-java-with-examples/ ]
                     System.out.print((char)j);
                     System.out.print('\b');
                 }
